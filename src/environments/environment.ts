1cb87f3878d168bcf8502f9739006d9c4c19fd75
Rafael <201903rafael@gmail.com>
2020-11-25 10:36:16 -0200

    alt. final login 25/11/2020

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+      apiKey: "AIzaSyBoxgGB2XMj8oKNSx2Fy_RExBdtCzzf_1M",
+    authDomain: "proj-final-lightpet.firebaseapp.com",
+    databaseURL: "https://proj-final-lightpet.firebaseio.com",
+    projectId: "proj-final-lightpet",
+    storageBucket: "proj-final-lightpet.appspot.com",
+    messagingSenderId: "657124352584",
+    appId: "1:657124352584:web:d146864f427ec90a8b2e7d",
+    measurementId: "G-1KG4HJHRZ4"
+    }
+  
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
