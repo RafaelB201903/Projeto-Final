652d492bf99f207325ddc22304d01c8f418d4c1f
Rafael <201903rafael@gmail.com>
2020-11-28 11:20:24 -0200

    versão

--- src/app/home-cliente/home-cliente.page.ts
+++ src/app/home-cliente/home-cliente.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-cliente',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeClientePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goPage() {
+    this.router.navigate(['/pedido-novo'])
   }
 
 }
