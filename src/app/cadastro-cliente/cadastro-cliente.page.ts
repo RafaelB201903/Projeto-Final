d29b24ebc1bc6f50ad8af62109e4c93cf57c77a8
Rafael <201903rafael@gmail.com>
2020-12-05 15:03:04 -0200

    Alt. semi-final 05/12/2020

--- src/app/cadastro-cliente/cadastro-cliente.page.ts
+++ src/app/cadastro-cliente/cadastro-cliente.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MenuController, ToastController, NavParams, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-cadastro-cliente',
@@ -8,10 +10,44 @@ import { MenuController } from '@ionic/angular';
 })
 export class CadastroClientePage implements OnInit {
 
-  constructor(private menuCtrl: MenuController) { }
+  @ViewChild('usuario') email;
+  @ViewChild('senha') senha;
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public firebaseauth: AngularFireAuth,
+    public toastCtrl : ToastController,
+    public menuCtrl: MenuController) {
+  }
 
   ngOnInit() {
     this.menuCtrl.enable(false);
+  }
+
+  
+
+  
+
+  ionViewDidLoad(){
+    this.menuCtrl.enable(false);
+  }
+  
+  cadastrar() {
+    this.firebaseauth.createUserWithEmailAndPassword(this.email.value,this.senha.value)
+      .then(()=> {
+        this.msgSucesso();
+      })
+      .catch(()=> {
+        this.msgErro();
+      })
+  }
+
+  msgSucesso() {
+    console.log("cadastrado com sucesso.")
+  }
+
+  msgErro() {
+    console.log("erro ao cadastrar.")
   }
 
 }
