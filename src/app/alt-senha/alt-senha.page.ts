5a7746452644ccf642488e15de2f6a95c80497c0
Rafael <201903rafael@gmail.com>
2021-02-26 15:18:13 -0300

    alterar senha finalizado

--- src/app/alt-senha/alt-senha.page.ts
+++ src/app/alt-senha/alt-senha.page.ts
@@ -1,5 +1,8 @@
+import { TemplateService } from 'src/app/services/template.service';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-alt-senha',
@@ -8,17 +11,53 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class AltSenhaPage implements OnInit {
 
-  constructor(private auth : AngularFireAuth) { 
-  
+  formGroup: FormGroup;
+  emailAddress: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AngularFireAuth,
+    public template : TemplateService
+    
+  ) { 
+    this.auth.currentUser.then(response=>{
+      this.emailAddress = response.email;
+      
+    })
   }
+
 
   ngOnInit() {
+    
+    
+  }
+  
+
+  recuperarSenha() {
+
+    
+     
+      var email = this.auth
+      console.log(email)
+      
+      console.log(this.emailAddress)
+
+      email.sendPasswordResetEmail(this.emailAddress).then(function () {
+
+       
+        
+        
+      }).catch(function (error) {
+     
+      
+     
+
+      });
+    
+    
+
+  }
+ 
+
   }
 
- 
- redefinirSenha(){
- 
- }
-
-
-}
